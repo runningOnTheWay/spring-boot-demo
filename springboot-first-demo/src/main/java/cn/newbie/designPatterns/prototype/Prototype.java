9ab413db2e7c4c7ff72e08d1575922b9f10fef5e
liyunlong <liyunlong0327>
2020-11-23 21:43:42 +0800

    增加原型模式

---
+++ springboot-first-demo/src/main/java/cn/newbie/designPatterns/prototype/Prototype.java
@@ -0,0 +1,5 @@
+package cn.newbie.designPatterns.prototype;
+
+public class Prototype {
+
+}
--- springboot-first-demo/src/main/java/cn/newbie/designPatterns/singleton/Test.java
+++ springboot-first-demo/src/main/java/cn/newbie/designPatterns/singleton/Test.java
@@ -1,9 +1,12 @@
 package cn.newbie.designPatterns.singleton;
 
+
 public class Test {
   public static void main(String[] args){
     HungrySingleton singletonPattern = HungrySingleton.getInstance();
     HungrySingleton singletonPattern1 = HungrySingleton.getInstance();
     singletonPattern1.showMessage();
+
+
   }
 }
